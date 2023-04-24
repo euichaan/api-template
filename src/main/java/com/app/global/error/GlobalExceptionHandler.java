791cb684e6d53bf09622582e9b7cdca96c51ec64
euichanhwang <pine6971@naver.com>
2023-04-24 22:29:10 +0900

    refactor: 메서드 체이닝을 한 줄 단위로 변경한다

--- src/main/java/com/app/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/app/global/error/GlobalExceptionHandler.java
@@ -22,10 +22,8 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(BindException.class)
 	protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
 		log.error("handleBindException", e);
-		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(),
-			e.getBindingResult());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(errorResponse);
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(), e.getBindingResult());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 	/**
 	 * 주로 @RequestParam enum으로 binding 못했을 경우 발생
@@ -34,10 +32,8 @@ public class GlobalExceptionHandler {
 	protected ResponseEntity<ErrorResponse>
 	handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
 		log.error("handleMethodArgumentTypeMismatchException", e);
-		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(),
-			e.getMessage());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-			.body(errorResponse);
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 	/**
 	 * 지원하지 않은 HTTP method 호출 할 경우 발생
@@ -46,8 +42,7 @@ public class GlobalExceptionHandler {
 	protected ResponseEntity<ErrorResponse>
 	handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
 		log.error("handleHttpRequestMethodNotSupportedException", e);
-		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED.toString(),
-			e.getMessage());
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.METHOD_NOT_ALLOWED.toString(), e.getMessage());
 		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
 	}
 
@@ -58,8 +53,7 @@ public class GlobalExceptionHandler {
 	protected ResponseEntity<ErrorResponse> handleConflict(BusinessException e) {
 		log.error("BusinessException", e);
 		ErrorResponse errorResponse = ErrorResponse.of(e.getErrorCode().getErrorCode(), e.getMessage());
-		return ResponseEntity.status(e.getErrorCode().getHttpStatus())
-			.body(errorResponse);
+		return ResponseEntity.status(e.getErrorCode().getHttpStatus()).body(errorResponse);
 	}
 	/**
 	 * 나머지 예외 발생
@@ -67,8 +61,7 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(Exception.class)
 	protected ResponseEntity<ErrorResponse> handleException(Exception e) {
 		log.error("Exception", e);
-		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
-			e.getMessage());
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.INTERNAL_SERVER_ERROR.toString(), e.getMessage());
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 	}
 }
