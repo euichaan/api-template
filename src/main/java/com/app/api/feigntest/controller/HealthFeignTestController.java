61bdcb55dd0c2b806b5cb1617a5751f66312e9f7
euichanhwang <pine6971@naver.com>
2023-04-25 13:47:30 +0900

    test: feignClient를 테스트한다

---
+++ src/main/java/com/app/api/feigntest/controller/HealthFeignTestController.java
@@ -0,0 +1,26 @@
+package com.app.api.feigntest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.api.feigntest.client.HelloClient;
+import com.app.api.health.dto.HealthCheckResponseDto;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class HealthFeignTestController {
+
+	private final HelloClient helloClient;
+
+	@GetMapping("/health/feign-test")
+	public ResponseEntity<HealthCheckResponseDto> healthCheckTest() {
+		HealthCheckResponseDto healthCheckResponseDto = helloClient.healthCheck();
+		return ResponseEntity.ok(healthCheckResponseDto);
+	}
+
+}
