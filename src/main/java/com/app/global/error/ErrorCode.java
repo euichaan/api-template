41f6a7781dd92341db8db3e77cf50b21465d3f30
euichanhwang <pine6971@naver.com>
2023-04-25 16:15:45 +0900

    feat: ErrorCode를 추가한다

--- src/main/java/com/app/global/error/ErrorCode.java
+++ src/main/java/com/app/global/error/ErrorCode.java
@@ -7,7 +7,11 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-	TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "business exception test");
+	TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "business exception test"),
+
+	// 인증
+	TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "A-001", "토큰이 만료되었습니다."),
+	NOT_VALID_TOKEN(HttpStatus.UNAUTHORIZED, "A-002", "해당 토큰은 유효한 토큰이 아닙니다.");
 
 	ErrorCode(final HttpStatus httpStatus, final String errorCode, final String message) {
 		this.httpStatus = httpStatus;
