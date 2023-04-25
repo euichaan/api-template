c31dd75ae31bd527879850eff3d7fc6b43d0d310
euichanhwang <pine6971@naver.com>
2023-04-25 15:32:13 +0900

    feat: 회원 도메인을 설계한다

---
+++ src/main/java/com/app/domain/member/entity/Member.java
@@ -0,0 +1,54 @@
+package com.app.domain.member.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.app.domain.common.BaseEntity;
+import com.app.domain.member.constant.MemberType;
+import com.app.domain.member.constant.Role;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberId;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 10)
+	private MemberType memberType;
+
+	@Column(unique = true, length = 50, nullable = false)
+	private String email;
+
+	@Column(length = 200)
+	private String password; // todo 폼 로그인 시 사용
+
+	@Column(nullable = false, length = 20)
+	private String memberName;
+
+	@Column(length = 200)
+	private String profile;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false, length = 10)
+	private Role role;
+
+	@Column(length = 250)
+	private String refreshToken;
+
+	private LocalDateTime tokenExpirationTime;
+}
