5b6e8ca4f8a8d0b7283a32258c67d79294436c42
euichanhwang <pine6971@naver.com>
2023-05-05 16:43:36 +0900

    feat: MemberInfoArgumentResolver 를 구현한다
    컨트롤러의 파라미터 중 특정 조건에 맞는 파라미터가 있다면, 요청에 들어온 값을 이용해 원하는 객체를 만들어 바인딩한다

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,12 +1,16 @@
 package com.app.global.config.web;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.app.global.interceptor.AuthenticationInterceptor;
+import com.app.global.resolver.memberinfo.MemberInfoArgumentResolver;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 public class WebConfig implements WebMvcConfigurer {
 
 	private final AuthenticationInterceptor authenticationInterceptor;
+	private final MemberInfoArgumentResolver memberInfoArgumentResolver;
 
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
@@ -39,5 +44,10 @@ public class WebConfig implements WebMvcConfigurer {
 				"/api/access-token/issue",
 				"api/logout",
 				"/api/health");
+	}
+
+	@Override
+	public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(memberInfoArgumentResolver);
 	}
 }
