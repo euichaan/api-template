addbee1604eedb3ea7ba466d6ea2064d61bdc00c
euichanhwang <pine6971@naver.com>
2023-05-05 16:49:37 +0900

    feat: MemberInfoController에서 MemberInfoArgumentResolver를 사용한다

--- src/main/java/com/app/api/member/controller/MemberInfoController.java
+++ src/main/java/com/app/api/member/controller/MemberInfoController.java
@@ -2,15 +2,14 @@ package com.app.api.member.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.api.member.dto.MemberInfoResponseDto;
 import com.app.api.member.service.MemberInfoService;
-import com.app.global.jwt.service.TokenManager;
+import com.app.global.resolver.memberinfo.MemberInfo;
+import com.app.global.resolver.memberinfo.MemberInfoDto;
 
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,13 +18,10 @@ import lombok.RequiredArgsConstructor;
 public class MemberInfoController {
 
 	private final MemberInfoService memberInfoService;
-	private final TokenManager tokenManager;
 
 	@GetMapping("/info")
-	public ResponseEntity<MemberInfoResponseDto> getMemberInfo(@RequestHeader("Authorization") String authorizationHeader) {
-		String accessToken = authorizationHeader.split(" ")[1];
-		Claims tokenClaims = tokenManager.getTokenClaims(accessToken);
-		Long memberId = Long.valueOf((Integer)tokenClaims.get("memberId"));
+	public ResponseEntity<MemberInfoResponseDto> getMemberInfo(@MemberInfo MemberInfoDto memberInfoDto) {
+		Long memberId = memberInfoDto.getMemberId();
 		MemberInfoResponseDto memberInfoResponseDto = memberInfoService.getMemberInfo(memberId);
 
 		return ResponseEntity.ok(memberInfoResponseDto);
